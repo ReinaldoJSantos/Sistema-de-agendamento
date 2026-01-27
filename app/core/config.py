dcb18366ce48d8aadb1204b8bc210f01b26bf060
reinaldoweb <reinaldoweb@hotmail.com>
2026-01-27 14:57:09 -0300

    bildando o projeto

--- app/core/config.py
+++ app/core/config.py
@@ -2,10 +2,21 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str = "db"
+    POSTGRES_PORT: int = 5432
 
-    ADMIN_EMAIL: str
-    ADMIN_PASSWORD: str
+    @property
+    def DATABASE_URL(self) -> str:
+        return (
+            f"postgresql+psycopg://{self.POSTGRES_USER}:"
+            f"{self.POSTGRES_PASSWORD}@"
+            f"{self.POSTGRES_HOST}:"
+            f"{self.POSTGRES_PORT}/"
+            f"{self.POSTGRES_DB}"
+        )
 
     class Config:
         env_file = ".env"
--- app/core/database.py
+++ app/core/database.py
@@ -1,8 +1,21 @@
+from datetime import time
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.core.config import settings
 
+DATABASE_URL = settings.DATABASE_URL
+
 engine = create_engine(settings.DATABASE_URL)
+
+for i in range(10):
+    try:
+        engine = create_engine(DATABASE_URL)
+        engine.connect()
+        print("✅ Banco conectado")
+        break
+    except Exception:
+        print("⏳ Aguardando banco...")
+        time.sleep(2)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
